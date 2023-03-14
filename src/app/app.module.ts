29fb48b5663fac9ee3a1f5d041e0f6c696502736
Ansh Kataria <anshkataria28@gmail.com>
2023-03-14 17:01:43 +0530

    hero created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeroComponent } from './hero/hero.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
+  constructor(private http: HttpClient) {}
 
+  downloadFile() {
+    const url =
+      ' https://drive.google.com/file/d/1BVYc-XCXiso80mno8p0gF45lkm38tUvM/view?usp=share_link'; // Replace with your file URL
+    this.http.get(url, { responseType: 'blob' }).subscribe((blob) => {
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = "Ansh's_Resume.pdf"; // Replace with your file name
+      link.click();
+    });
+  }
 }
